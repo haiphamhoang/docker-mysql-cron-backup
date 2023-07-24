469e6244cc4430bd9cb550e06dec04853812f622
Hai Pham Hoang <33831192+haiphamhoang@users.noreply.github.com>
2023-07-24 12:58:59 +0700

    update disable log

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tail -F /mysql_backup.log &
 
-if [ "${INIT_BACKUP}" -gt "0" ]; then
+if [ "${INIT_BACKUP:-0}" -gt "0" ]; then
   echo "=> Create a backup on the startup"
   /backup.sh
 elif [ -n "${INIT_RESTORE_LATEST}" ]; then
@@ -11,10 +11,32 @@ elif [ -n "${INIT_RESTORE_LATEST}" ]; then
       echo "waiting database container..."
       sleep 1
   done
-  find /backup -maxdepth 1 -name '*.sql.gz' | tail -1 | xargs /restore.sh
+  # Needed to exclude the 'latest.<database>.sql.gz' file, consider only filenames starting with number
+  # Only data-tagged backups, eg. '202212250457.database.sql.gz', must be trapped by the regex
+  find /backup -maxdepth 1 -name '[0-9]*.*.sql.gz' | sort | tail -1 | xargs /restore.sh
 fi
 
-echo "${CRON_TIME} /backup.sh >> /mysql_backup.log 2>&1" > /tmp/crontab.conf
+function final_backup {
+    echo "=> Captured trap for final backup"
+    echo "=> Requested last backup at $(date "+%Y-%m-%d %H:%M:%S")"
+    exec /backup.sh
+    exit 0
+}
+
+if [ -n "${EXIT_BACKUP}" ]; then
+  echo "=> Listening on container shutdown gracefully to make last backup before close"
+  trap final_backup SIGHUP SIGINT SIGTERM
+fi
+
+touch /HEALTHY.status
+
+echo "${CRON_TIME} /backup.sh >> /mysqlcron_backup.log 2>&1" > /tmp/crontab.conf
 crontab /tmp/crontab.conf
 echo "=> Running cron task manager in foreground"
-exec crond -f -l 8 -L /mysql_backup.log
+crond -f -l 8 -L /mysql_backup.log &
+
+echo "Listening on crond, and wait..."
+
+tail -f /dev/null & wait $!
+
+echo "Script is shutted down."
